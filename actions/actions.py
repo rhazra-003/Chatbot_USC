4ab5af1ec2ff771804cfec35f5c39694eaba2eb3
Ridam Hazra <rhazra0602@gmail.com>
2021-07-13 08:38:50 +0530

    First commit

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,47 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+
+import rasa.core.tracker_store
+from rasa.shared.core.trackers import DialogueStateTracker
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionSaveConversation(Action):
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def name(self) -> Text:
+        return "action_save_conversation"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        conversation=tracker.events
+        print(conversation)
+        import os
+        if not os.path.isfile('chats.csv'):
+            with open('chats.csv','w') as file:
+                file.write("intent,user_input,entity_name,entity_value,action,bot_reply\n")
+        chat_data=''
+        for i in conversation:
+            if i['event'] == 'user':
+                chat_data+=i['parse_data']['intent']['name']+','+i['text']+','
+                print('user: {}'.format(i['text']))
+                if len(i['parse_data']['entities']) > 0:
+                    chat_data+=i['parse_data']['entities'][0]['entity']+','+i['parse_data']['entities'][0]['value']+','
+                    print('extra data:', i['parse_data']['entities'][0]['entity'], '=',
+                          i['parse_data']['entities'][0]['value'])
+                else:
+                    chat_data+=",,"
+            elif i['event'] == 'bot':
+                print('Bot: {}'.format(i['text']))
+                try:
+                    chat_data+=i['metadata']['utter_action']+','+i['text']+'\n'
+                except KeyError:
+                    pass
+        else:
+            with open('chats.csv','a') as file:
+                file.write(chat_data)
+
+        dispatcher.utter_message(text="")
+
+        return []
